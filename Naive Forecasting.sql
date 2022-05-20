0832b1af2855b81de7f9a300624d94647daa3659
Marcus <85585402+mcmitchell32@users.noreply.github.com>
2022-05-20 11:29:08 -0400

    Update and rename Naive Forecasting to Naive Forecasting.sql

--- Naive Forecasting.sql
+++ Naive Forecasting.sql
@@ -1,4 +1,4 @@
-Some forecasting methods are extremely simple and surprisingly effective. Naïve forecast is one of them; 
+'''Some forecasting methods are extremely simple and surprisingly effective. Naïve forecast is one of them; 
 we simply set all forecasts to be the value of the last observation. Our goal is to develop a naïve forecast for a new metric 
 called "distance per dollar" defined as the (distance_to_travel/monetary_cost) in our dataset and measure its accuracy.
 
@@ -6,10 +6,8 @@ To develop this forecast,  sum "distance to travel"  and "monetary cost" values 
 This value becomes your actual value for the current month. The next step is to populate the forecasted value for each month. 
 This can be achieved simply by getting the previous month's value in a separate column. Now, we have actual and forecasted values. 
 This is your naïve forecast. Let’s evaluate our model by calculating an error matrix called root mean squared error (RMSE). 
-RMSE is defined as sqrt(mean(square(actual - forecast)). Report out the RMSE rounded to the 2nd decimal spot.
+RMSE is defined as sqrt(mean(square(actual - forecast)). Report out the RMSE rounded to the 2nd decimal spot.'''
 
-SQL: 
------------------------------------------
 WITH cte AS (
 SELECT TO_CHAR(request_date::DATE, 'YYYY-MM') AS year_month, 
        SUM(distance_to_travel) / SUM(monetary_cost) AS actual, 
@@ -23,34 +21,3 @@ SELECT SQRT(AVG(POWER((actual - forecast), 2))) AS RMSE
 
 SELECT  ROUND(RMSE::DECIMAL, 2) AS RMSE
     FROM RMSE 
-
-PYTHON: 
------------------------------------------
-df = uber_request_logs 
-
-df['year_month'] = pd.to_datetime(df['request_date']).dt.to_period('M') 
-
-df = df.groupby(['year_month']).agg({'distance_to_travel': 'sum', 'monetary_cost': 'sum'}).reset_index() 
-
-df['actual'] = df['distance_to_travel'] / df['monetary_cost'] 
-
-df['forecast'] = df['actual'].shift(1) 
-
-RMSE = (df['actual'] - df['forecast'])**2 
-
-RMSE = round(RMSE.mean()**0.5, 2)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
