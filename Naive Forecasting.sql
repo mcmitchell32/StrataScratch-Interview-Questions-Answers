c12430cb0949f5d3296904c754e7ce99ba60907d
Marcus <85585402+mcmitchell32@users.noreply.github.com>
2022-05-20 11:29:34 -0400

    Update Naive Forecasting.sql

--- Naive Forecasting.sql
+++ Naive Forecasting.sql
@@ -1,12 +1,12 @@
-'''Some forecasting methods are extremely simple and surprisingly effective. Naïve forecast is one of them; 
-we simply set all forecasts to be the value of the last observation. Our goal is to develop a naïve forecast for a new metric 
-called "distance per dollar" defined as the (distance_to_travel/monetary_cost) in our dataset and measure its accuracy.
+--Some forecasting methods are extremely simple and surprisingly effective. Naïve forecast is one of them; 
+--we simply set all forecasts to be the value of the last observation. Our goal is to develop a naïve forecast for a new metric 
+--called "distance per dollar" defined as the (distance_to_travel/monetary_cost) in our dataset and measure its accuracy.
 
-To develop this forecast,  sum "distance to travel"  and "monetary cost" values at a monthly level before calculating "distance per dollar". 
-This value becomes your actual value for the current month. The next step is to populate the forecasted value for each month. 
-This can be achieved simply by getting the previous month's value in a separate column. Now, we have actual and forecasted values. 
-This is your naïve forecast. Let’s evaluate our model by calculating an error matrix called root mean squared error (RMSE). 
-RMSE is defined as sqrt(mean(square(actual - forecast)). Report out the RMSE rounded to the 2nd decimal spot.'''
+--To develop this forecast,  sum "distance to travel"  and "monetary cost" values at a monthly level before calculating "distance per dollar". 
+--This value becomes your actual value for the current month. The next step is to populate the forecasted value for each month. 
+--This can be achieved simply by getting the previous month's value in a separate column. Now, we have actual and forecasted values. 
+--This is your naïve forecast. Let’s evaluate our model by calculating an error matrix called root mean squared error (RMSE). 
+--RMSE is defined as sqrt(mean(square(actual - forecast)). Report out the RMSE rounded to the 2nd decimal spot.
 
 WITH cte AS (
 SELECT TO_CHAR(request_date::DATE, 'YYYY-MM') AS year_month, 
