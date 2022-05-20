1b145da80b8097f9a43e98b85c1714489ffbb8c6
Marcus <85585402+mcmitchell32@users.noreply.github.com>
2022-05-20 11:20:13 -0400

    Update and rename Monthly Percentage Difference to Monthly Percentage Difference.sql

---
+++ Monthly Percentage Difference.sql
@@ -0,0 +1,16 @@
+--Given a table of purchases by date, calculate the month-over-month percentage change in revenue. 
+--The output should include the year-month date (YYYY-MM) and percentage change, rounded to the 2nd decimal point, and sorted from the beginning of the year to the end of the year.
+--The percentage change column will be populated from the 2nd month forward and can be calculated as ((this month's revenue - last month's revenue) / last month's revenue)*100.
+
+WITH cte AS ( 
+SELECT TO_CHAR(created_at::DATE, 'YYYY-MM') AS year_month, 
+       SUM(value) AS total_revenue, 
+       LAG(SUM(value), 1) OVER (ORDER BY TO_CHAR(created_at::DATE, 'YYYY-MM') ASC) AS previous_revenue
+  FROM sf_transactions 
+GROUP BY year_month) 
+
+SELECT year_month, 
+       ROUND(((total_revenue - previous_revenue) / previous_revenue) * 100, 2) AS pct_change
+  FROM cte 
+
+
